5a0f8abbb9b615bc6690d884fe6f1fc039d7e26a
Phil Hayes <philipehayes@bravendary.com>
2019-07-11 22:00:36 -0500

    update project and survey types

--- Arc/Components/Survey/Models/ACSurveyType.swift
+++ Arc/Components/Survey/Models/ACSurveyType.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public enum SurveyType : String, Codable {
-	case unknown, auth, ema, edna, mindfulness, schedule, mindfulnessReminder, context, finished, finishedNoQuestions, gridTest, priceTest, symbolsTest, cognitive, wake, chronotype, region, language
+	case unknown, auth, ema, edna, mindfulness, schedule, mindfulnessReminder, context, finished, finishedNoQuestions, gridTest, priceTest, symbolsTest, cognitive, wake, chronotype, region, language, onboarding
 
 	public var metatype: HMCodable.Type {
 		switch self {
