18594b9ddb69a5ec948fad63f43a2affa4e1406a
Phil Hayes <phil@thinkhappymedium.com>
2019-08-13 13:07:40 -0500

    Progress Tab layout.

--- Arc/Components/Progress/views/ACProgressView.swift
+++ Arc/Components/Progress/views/ACProgressView.swift
@@ -9,12 +9,13 @@
 import UIKit
 import ArcUIKit
 class ACProgressView: ACTemplateView {
-
+	private var startDay:Date?
 	override func content(_ view: UIView) {
 		if let v = view as? UIStackView {
 			v.layoutMargins = .zero
 		}
 		view.stack {
+			let stack = $0
 			$0.axis = .vertical
 			$0.isLayoutMarginsRelativeArrangement = true
 			$0.layoutMargins = UIEdgeInsets(top: 24, left: 0, bottom: 24, right: 0)
@@ -109,19 +110,160 @@ class ACProgressView: ACTemplateView {
 						$0.text = "Day *6* of *7*".localized("progess_weeklystatus")
 					}
 					
-					
-					
-					
-					$0.stepperProgress {
-						
-						$0.config.foregroundColor = #colorLiteral(red: 0, green: 0.3729999959, blue: 0.5220000148, alpha: 1)
-						$0.config.endRadius = 25
 
-						$0.layout {
-							$0.height == 50 ~ 999
+					$0.weekStepperProgress {
+						$0.set(step: 0, of: ["S", "M", "T", "W", "T", "F", "S"])
+					}
+					
+					$0.stack {
+						$0.axis = .vertical
+						$0.spacing = 8.0
+						$0.acLabel {
+							Roboto.Style.body($0, color: ACColor.primary)
+							$0.text = "".localized(ACTranslationKey.progress_startdate)
+							
+						}
+						$0.acLabel {
+							Roboto.Style.body($0)
+							$0.text = Date().localizedFormat(template: ACDateStyle.longWeekdayMonthDay.rawValue,
+															 options: 0,
+															 locale: nil)
 						}
 					}
 					
+					$0.stack {
+						$0.axis = .vertical
+						$0.spacing = 8.0
+
+						
+						$0.acLabel {
+							Roboto.Style.body($0, color: ACColor.primary)
+							$0.text = "".localized(ACTranslationKey.progress_enddate)
+						}
+						$0.acLabel {
+							Roboto.Style.body($0)
+							$0.text = Date().addingDays(days: 6).localizedFormat(template: ACDateStyle.longWeekdayMonthDay.rawValue,
+																				 options: 0,
+																				 locale: nil)
+						}
+					}
+					
+				}
+			}
+			$0.view {
+				
+				//This Week
+				$0.backgroundColor = ACColor.primaryInfo
+				$0.stack {
+					
+					$0.attachTo(view: $0.superview)
+					$0.axis = .vertical
+					$0.alignment = .fill
+					$0.spacing = 20
+					$0.isLayoutMarginsRelativeArrangement = true
+					$0.layoutMargins = UIEdgeInsets(top: 24, left: 24, bottom: 24, right: 24)
+					$0.acLabel {
+						Roboto.Style.headingMedium($0, color: .white)
+						$0.text = "This Week".localized("progress_weekly_header")
+						
+					}
+					
+					$0.acHorizontalBar {
+						$0.relativeWidth = 0.15
+						$0.color = UIColor(named: "HorizontalSeparator")
+						$0.layout {
+							$0.height == 2 ~ 999
+							
+						}
+					}
+					
+					$0.stack {
+						$0.acLabel {
+							Roboto.Style.subHeading($0, color: .white)
+							$0.text = "Day *6* of *7*".localized(ACTranslationKey.progress_studystatus)
+						}
+					}
+					
+					
+					
+					$0.weekStepperProgress {
+						$0.set(step: 0, of: ["S", "M", "T", "W", "T", "F", "S"])
+					}
+					
+					$0.stack {
+						$0.axis = .vertical
+						$0.spacing = 8.0
+						$0.acLabel {
+							Roboto.Style.body($0, color: ACColor.highlight)
+							$0.text = "".localized(ACTranslationKey.progress_joindate)
+							
+						}
+						$0.acLabel {
+							Roboto.Style.body($0, color: .white)
+							$0.text = Date().localizedFormat(template: ACDateStyle.longWeekdayMonthDay.rawValue,
+															 options: 0,
+															 locale: nil)
+						}
+					}
+					
+					$0.stack {
+						$0.axis = .vertical
+						$0.alignment = .leading
+						$0.spacing = 8.0
+						
+						$0.stack {
+							$0.axis = .horizontal
+							$0.distribution = .equalSpacing
+							$0.acLabel {
+								Roboto.Style.body($0, color: ACColor.highlight)
+								$0.text = "".localized(ACTranslationKey.progess_finishdate)
+								$0.numberOfLines = 1
+
+							}
+							$0.acLabel {
+								Roboto.Style.body($0, color: ACColor.badgeBackground)
+								$0.text = "".localized(ACTranslationKey.footnote_symbol)
+								$0.numberOfLines = 1
+							}
+						}
+						$0.acLabel {
+							Roboto.Style.body($0, color:.white)
+							$0.text = Date().addingDays(days: 6).localizedFormat(template: ACDateStyle.longWeekdayMonthDay.rawValue,
+																				 options: 0,
+																				 locale: nil)
+						}
+					}
+					
+					$0.stack {
+						$0.axis = .vertical
+						$0.spacing = 8.0
+						
+						
+						$0.acLabel {
+							Roboto.Style.body($0, color: ACColor.highlight)
+							$0.text = "".localized(ACTranslationKey.progress_timebtwtesting)
+						}
+						$0.acLabel {
+							
+							var components = DateComponents()
+							var calendar = Calendar.current
+							calendar.locale = Locale(identifier: Arc.shared.appController.locale.string)
+							components.month = 3
+							
+							DateComponentsFormatter.localizedString(from: components, unitsStyle: DateComponentsFormatter.UnitsStyle.full)
+							Roboto.Style.body($0, color:.white)
+							$0.text = Date().addingDays(days: 6).localizedFormat(template: ACDateStyle.longWeekdayMonthDay.rawValue,
+																				 options: 0,
+																				 locale: nil)
+						}
+					}
+					
+					$0.acButton {
+						$0.primaryColor = ACColor.secondary
+						$0.secondaryColor = ACColor.secondaryGradient
+						$0.setTitleColor(ACColor.badgeText, for: .normal)
+						$0.setTitle("".localized(ACTranslationKey.button_viewfaq), for: .normal)
+					}
 					
 				}
 			}
--- Arc/Components/Progress/views/supporting views/StepperProgressview.swift
+++ Arc/Components/Progress/views/supporting views/StepperProgressview.swift
@@ -5,28 +5,23 @@ public class StepperProgressView : UIView {
 		public var foregroundColor:UIColor = .blue
 		public var backgroundColor:UIColor = .lightGray
 		
-		public var barInset:CGFloat = 8
+		public var barInset:CGFloat = 10
 		public var barWidth:CGFloat = 32
-		public var endRadius:CGFloat = 25
 		public var outlineColor:UIColor = .black
 		
 	}
 	public var progress:CGFloat = 0.5
-	public var config:Config = Config() {
-		didSet {
-			print("updating")
-			outlinePath = UIBezierPath(roundedRect: bounds.insetBy(dx:0.0, dy: config.barInset), cornerRadius: bounds.height/2.0)
-			setNeedsDisplay()
-		}
-	}
+	public var pos:CGFloat?
+	public var config:Config = Config()
 	
 	var outlinePath:UIBezierPath = UIBezierPath()
 	var fillPath:UIBezierPath = UIBezierPath()
+	
+	
 	public override init(frame: CGRect) {
 		super.init(frame: frame)
 		backgroundColor = .clear
-		outlinePath = UIBezierPath(roundedRect: bounds.insetBy(dx:0.0, dy: config.barInset), cornerRadius: bounds.height/2.0)
-		config.endRadius = frame.height/2.0
+		
 		
 	}
 	
@@ -40,8 +35,14 @@ public class StepperProgressView : UIView {
 		
 		let context = UIGraphicsGetCurrentContext()
 		context?.setStrokeColor(config.outlineColor.cgColor)
+		outlinePath = UIBezierPath(roundedRect: bounds.insetBy(dx:1.0, dy: config.barInset), cornerRadius: bounds.height/2.0)
+
+		outlinePath.lineWidth = 1.0
 		outlinePath.stroke()
-		let endRect = CGRect(x: (frame.width -  config.endRadius * 2) * progress, y: rect.midY - config.endRadius, width: config.endRadius * 2, height: config.endRadius * 2)
+		var endRect = CGRect(x: (frame.width -  frame.height) * progress, y: rect.midY - frame.height/2.0, width: frame.height, height: frame.height)
+		if let pos = pos {
+			endRect = CGRect(x: pos - frame.height/2, y: rect.midY - frame.height/2.0, width: frame.height, height: frame.height)
+		}
 		fillPath.move(to: CGPoint(x: rect.minX + config.barWidth / 2.0, y: rect.midY))
 		fillPath.addLine(to: CGPoint(x: endRect.midX , y: endRect.midY))
 		context?.setStrokeColor(config.foregroundColor.cgColor)
