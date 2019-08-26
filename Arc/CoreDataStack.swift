a429e449fa2ad2e3dac411a6ee7211ee98e16921
Phil Hayes <phil@thinkhappymedium.com>
2019-08-26 10:10:14 -0500

    External reinitializer to allow unit test to clear all data before proceeding to next test.

--- Arc/CoreDataStack.swift
+++ Arc/CoreDataStack.swift
@@ -33,24 +33,28 @@ open class CoreDataStack {
     
     lazy public var mockPersistantContainer: NSPersistentContainer = {
         
-        let container = NSPersistentContainer(name: "arc", managedObjectModel: self.managedObjectModel)
-        let description = NSPersistentStoreDescription()
-        description.type = NSInMemoryStoreType
-        description.shouldAddStoreAsynchronously = false // Make it simpler in test env
-        
-        container.persistentStoreDescriptions = [description]
-        container.loadPersistentStores { (description, error) in
-            // Check if the data store is in memory
-            precondition( description.type == NSInMemoryStoreType )
-            
-            // Check if creating container wrong
-            if let error = error {
-                fatalError("Create an in-mem coordinator failed \(error)")
-            }
-        }
-        return container
+		
+        return initializeMockStore()
     }()
-    
+	public func initializeMockStore() -> NSPersistentContainer {
+		CoreDataStack.useMockContainer = true
+		let container = NSPersistentContainer(name: "arc", managedObjectModel: self.managedObjectModel)
+		let description = NSPersistentStoreDescription()
+		description.type = NSInMemoryStoreType
+		description.shouldAddStoreAsynchronously = false // Make it simpler in test env
+		
+		container.persistentStoreDescriptions = [description]
+		container.loadPersistentStores { (description, error) in
+			// Check if the data store is in memory
+			precondition( description.type == NSInMemoryStoreType )
+			
+			// Check if creating container wrong
+			if let error = error {
+				fatalError("Create an in-mem coordinator failed \(error)")
+			}
+		}
+		return container
+	}
     lazy public var persistentContainer: NSPersistentContainer = {
 		
 		if ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil || CoreDataStack.useMockContainer{
