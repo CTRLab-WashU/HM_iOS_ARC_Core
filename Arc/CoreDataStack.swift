b7fd17eedb6fb35a07ab33916855a2d0334f7059
Phil Hayes <phil@thinkhappymedium.com>
2019-08-27 10:23:27 -0500

    wrap context in serial queue

--- Arc/CoreDataStack.swift
+++ Arc/CoreDataStack.swift
@@ -101,22 +101,24 @@ open class CoreDataStack {
         return managedObjectModel
     }()
     public func saveContext () {
-        guard !CoreDataStack.isSaving else {
-            
-//            print("Blocked save")
-            return
-            
-        }
-//        print("Starting save....")
-
-        CoreDataStack.isSaving = true
-        var context = persistentContainer.viewContext
-        if context.hasChanges {
-//            context.performAndWait {
-
+//        guard !CoreDataStack.isSaving else {
+//
+////            print("Blocked save")
+//            return
+//
+//        }
+////        print("Starting save....")
+//
+//        CoreDataStack.isSaving = true
+		
+		DispatchQueue.main.async { [unowned self] in
+			var context = self.persistentContainer.viewContext
+			if context.hasChanges {
+				//            context.performAndWait {
+				
 				do {
-						try context.save()
-
+					try context.save()
+					
 					
 				} catch {
 					// Replace this implementation with code to handle the error appropriately.
@@ -125,26 +127,28 @@ open class CoreDataStack {
 					assertionFailure("Unresolved error \(nserror), \(nserror.userInfo)")
 				}
 			}
-//        }
-		context = MHController.dataContext
-		if context.hasChanges {
-//            context.performAndWait {
-
+			//        }
+			context = MHController.dataContext
+			if context.hasChanges {
+				//            context.performAndWait {
+				
 				do {
-
-						try context.save()
-
+					
+					try context.save()
+					
 				} catch {
 					// Replace this implementation with code to handle the error appropriately.
 					// fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
 					let nserror = error as NSError
 					assertionFailure("Unresolved error \(nserror), \(nserror.userInfo)")
 				}
-//            }
+				//            }
+			}
 		}
+		
 //        print("Ending save.")
 
-        CoreDataStack.isSaving = false
+//        CoreDataStack.isSaving = false
 
     }
 }
