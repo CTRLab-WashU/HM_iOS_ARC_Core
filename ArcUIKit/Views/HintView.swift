f0a0a34b3b5bd173e9fad91afb432c763055e0d8
Spencer King <spencer@thinkhappymedium.com>
2019-09-05 10:26:23 -0500

    hints > allow arrows to be drawn above and below the hint

--- ArcUIKit/Views/HintView.swift
+++ ArcUIKit/Views/HintView.swift
@@ -77,7 +77,8 @@ public class HintView : IndicatorView {
 											 secondaryColor: UIColor(named:"HintFill")!,
 											 textColor: .black,
 											 cornerRadius: 8.0,
-											 arrowEnabled: false))
+											 arrowEnabled: false,
+                                             arrowAbove: false))
 	}
 	required public init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
