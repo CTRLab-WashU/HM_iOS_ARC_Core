66b358f39a3d5fbe1c4a474f30f5f4e0ea429c45
Spencer King <spencer@thinkhappymedium.com>
2019-09-11 10:58:43 -0500

    EXR-361, update Tour the App coachmark style and position

--- Arc/Components/Home/ViewControllers/ACHomeViewController.swift
+++ Arc/Components/Home/ViewControllers/ACHomeViewController.swift
@@ -58,18 +58,19 @@ open class ACHomeViewController: CustomViewController<ACHomeView> {
     
     // Shown after the baseline test
     func showBaselineOboarding() {
-        view.window?.overlayView(withShapes: [])
+        // view.window?.overlayView(withShapes: [])
         view.isUserInteractionEnabled = false
         currentHint = view.window?.hint {
             $0.layout {
                 $0.centerX == view.centerXAnchor
-                $0.centerY == view.centerYAnchor
+                $0.centerY == view.bottomAnchor - 60
                 $0.width == 232
-                $0.height == 146
+                $0.height == 73
             }
             
-            //$0.content = "".localized(ACTranslationKey.popup_nicejob)
+            $0.content = nil
             $0.buttonTitle = "".localized(ACTranslationKey.popup_tour)
+            $0.hideBar()
             $0.onTap = {[unowned self] in
                 self.set(flag: .baseline_onboarding)
                 self.currentHint?.removeFromSuperview()
@@ -82,17 +83,18 @@ open class ACHomeViewController: CustomViewController<ACHomeView> {
     
     // Shown after paid tests if the user has not previously completed the onboarding flow
     func showPaidOnboarding() {
-        view.window?.overlayView(withShapes: [])
+        // view.window?.overlayView(withShapes: [])
         view.isUserInteractionEnabled = false
         currentHint = view.window?.hint {
             $0.layout {
                 $0.centerX == view.centerXAnchor
-                $0.centerY == view.centerYAnchor
+                $0.centerY == view.bottomAnchor - 60
                 $0.width == 232
-                $0.height == 146
+                $0.height == 73
             }
-            $0.content = "".localized(ACTranslationKey.popup_nicejob)
+            $0.content = nil
             $0.buttonTitle = "".localized(ACTranslationKey.popup_next)
+            $0.hideBar()
             $0.onTap = {[unowned self] in
                 self.set(flag: .baseline_onboarding)
                 self.currentHint?.removeFromSuperview()
