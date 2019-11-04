5f284b1a0020a3721818bff2e9bc1cc00b4ecb33
Andrew Pearson <andrew@thinkhappymedium.com>
2019-11-04 13:28:34 -0600

    EXR-300 forgot to include function

--- Arc/App/Views/InfoView.swift
+++ Arc/App/Views/InfoView.swift
@@ -100,6 +100,9 @@ public class InfoView: ACTemplateView {
 	public func setHeading(_ text:String?) {
 		infoContent.setHeader(text)
 	}
+    public func setIntroHeading(_ text:String?){
+        infoContent.setIntroHeader(text)
+    }
 	public func setSeparatorWidth(_ width:CGFloat) {
 		infoContent.setSeparatorWidth(width)
 	}
