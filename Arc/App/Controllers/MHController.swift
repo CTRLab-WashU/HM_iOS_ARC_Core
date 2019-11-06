9ece6d80b09b29247b1fbd29adf86e0ab6576570
Phil Hayes <phil@thinkhappymedium.com>
2019-11-06 09:41:58 -0600

    Run all fetch operations through the background context. This is intended to prevent random crashes because of cross context references.

--- Arc/App/Controllers/MHController.swift
+++ Arc/App/Controllers/MHController.swift
@@ -97,20 +97,27 @@ open class MHController {
 		return item
 	}
     public func fetch<T:NSManagedObject>(predicate:NSPredicate? = nil, sort:[NSSortDescriptor]? = nil, limit:Int? = nil) -> [T]? {
-        do {
-            if let fetchRequest:NSFetchRequest<T> = T.fetchRequest() as? NSFetchRequest<T> {
-                fetchRequest.predicate = predicate
-                fetchRequest.sortDescriptors = sort
-                if let limit = limit {
-                    fetchRequest.fetchLimit = limit
-                }
-                let results = try MHController.dataContext.fetch(fetchRequest)
-                return results
-            }
-        }  catch {
-            delegate?.didCatch(errors: error)
-        }
-        return nil
+		var results:[T]? = nil
+		
+		if let fetchRequest:NSFetchRequest<T> = T.fetchRequest() as? NSFetchRequest<T> {
+			fetchRequest.predicate = predicate
+			fetchRequest.sortDescriptors = sort
+			if let limit = limit {
+				fetchRequest.fetchLimit = limit
+			}
+			MHController.dataContext.performAndWait {
+				do {
+					results = try MHController.dataContext.fetch(fetchRequest)
+				}  catch {
+						delegate?.didCatch(errors: error)
+				}
+			}
+		}
+			
+			
+		
+		return results
+		
     }
     
     public func get<T:HMCodable>(id:String) throws -> T {
