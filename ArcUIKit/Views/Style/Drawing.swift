2665443105e3ae0ad5375394ad71c5142cba21bd
Phil Hayes <phil@thinkhappymedium.com>
2019-12-11 14:46:22 -0600

    Add draw inner shaddow functionality.

--- ArcUIKit/Views/Style/Drawing.swift
+++ ArcUIKit/Views/Style/Drawing.swift
@@ -12,6 +12,38 @@ public protocol ACDrawable{
 	func draw(_ rect:CGRect)
 }
 public struct Drawing {
+	
+	public static func drawInnerShadow(context:CGContext,
+									   path:CGPath,
+									   shadowColor:CGColor,
+									   offSet:CGSize,
+									   blurRadius:CGFloat) {
+		
+		guard let opaqueShadowColor = shadowColor.copy(alpha: 1.0) else {
+			assertionFailure("Color could not be copied.")
+			return
+		}
+		context.saveGState()
+		
+			context.addPath(path)
+		
+			context.clip()
+		
+		
+			context.setAlpha(shadowColor.alpha)
+		
+			context.beginTransparencyLayer(auxiliaryInfo: nil)
+				context.setShadow(offset: offSet, blur: blurRadius, color: opaqueShadowColor)
+				context.setBlendMode(.sourceOut)
+				context.setFillColor(opaqueShadowColor)
+				context.addPath(path)
+				context.fillPath()
+			context.endTransparencyLayer()
+		context.restoreGState()
+		
+		
+	}
+	
 	public struct CircularBar : ACDrawable {
 		private var startAngle = (3.0 * CGFloat.pi)/2.0
 		private var endAngle = 2.0 * CGFloat.pi
@@ -216,6 +248,10 @@ public struct Drawing {
 			let startPoint = CGPoint.zero
 			let endPoint = CGPoint(x:0, y:rect.height)
 			context?.drawLinearGradient(gradient, start: startPoint, end: endPoint, options:[])
+			guard let c = context else {
+				return
+			}
+//			Drawing.drawInnerShadow(context: c, path: path.cgPath, shadowColor: UIColor.red.cgColor, offSet: CGSize(width: 2.0, height: 0.0), blurRadius: 5.0)
 		}
 	}
 
