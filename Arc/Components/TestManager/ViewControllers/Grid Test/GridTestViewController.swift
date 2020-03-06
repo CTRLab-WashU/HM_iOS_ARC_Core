4d741e7e1089c329935f2625ef149876845d72a4
Phil Hayes <phil@thinkhappymedium.com>
2020-03-06 14:43:24 -0600

    update tags for next test loader.

--- Arc/Components/TestManager/ViewControllers/Grid Test/GridTestViewController.swift
+++ Arc/Components/TestManager/ViewControllers/Grid Test/GridTestViewController.swift
@@ -308,9 +308,8 @@ open class GridTestViewController: ArcViewController, UICollectionViewDelegate, 
         if testNumber >= controller.get(testCount: responseId)
         {
 			_ = controller.mark(filled: responseId)
-			let nextMessage = (ACState.testCount == 3) ? "Well done!" : "Loading next test..."
-			
-			let vc = TestProgressViewController(title: "Grids Test Complete!", subTitle: nextMessage, count: ACState.testTaken - 1)
+			let nextMessage = (ACState.testCount == 3) ? "Well done!".localized(ACTranslationKey.testing_done) : "Loading next test...".localized(ACTranslationKey.testing_loading)
+			let vc = TestProgressViewController(title: "Symbols Test Complete!".localized(ACTranslationKey.grids_complete), subTitle: nextMessage, count: ACState.testTaken - 1)
 			vc.delegate = self
 			self.addChild(vc)
 			self.view.anchor(view: vc.view)
