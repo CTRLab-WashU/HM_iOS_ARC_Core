9396086d22d8f0686796eb3835fb5520fd626c0c
Andrew Pearson <andrew@thinkhappymedium.com>
2020-11-05 12:49:04 -0600

    remove change hint if selected cell is deselected

--- Arc/Components/TestManager/ViewControllers/Grid Test/ExtendedGridTestViewController.swift
+++ Arc/Components/TestManager/ViewControllers/Grid Test/ExtendedGridTestViewController.swift
@@ -560,6 +560,7 @@ open class ExtendedGridTestViewController: ArcViewController, UICollectionViewDe
             //guard !isPracticeTest else { return }
             
             choiceIndicator?.removeFromSuperview()
+            delegate?.didUpdateIndicator(indexPath: indexPath, indicator: nil)
 //            choiceIndicator?.targetView?.backgroundColor = UIColor(red: 191.0/255.0, green: 215.0/255.0, blue: 224.0/255.0, alpha: 1.0)
 //            choiceIndicator?.targetView?.layer.borderWidth = 1
 //            choiceIndicator?.targetView?.layer.borderColor = UIColor(red: 133.0/255.0, green: 141/255.0, blue: 145.0/255.0, alpha: 1.0).cgColor
