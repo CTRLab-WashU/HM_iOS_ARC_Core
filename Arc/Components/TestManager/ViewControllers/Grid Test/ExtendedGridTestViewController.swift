ebfc4b19d239930ab9e7f1a64873ae4aa82417ab
Andrew Pearson <andrew@thinkhappymedium.com>
2020-11-17 14:50:47 -0600

    A20U-48 changed grids version b timing from 20 to 30 seconds

--- Arc/Components/TestManager/ViewControllers/Grid Test/ExtendedGridTestViewController.swift
+++ Arc/Components/TestManager/ViewControllers/Grid Test/ExtendedGridTestViewController.swift
@@ -317,7 +317,7 @@ open class ExtendedGridTestViewController: ArcViewController, UICollectionViewDe
         _ = controller.markTime(gridDisplayedTestGrid: responseId, questionIndex: testNumber)
 		if shouldAutoProceed {
 
-        	endTimer = Timer.scheduledTimer(timeInterval: 20, target: self, selector: #selector(endTest), userInfo: nil, repeats: false)
+        	endTimer = Timer.scheduledTimer(timeInterval: 30, target: self, selector: #selector(endTest), userInfo: nil, repeats: false)
 		}
         
         view.layoutSubviews()
