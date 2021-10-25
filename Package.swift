a4104c20a7d0bc3fa6b926983b524ac5da992a1f
Mike DePhillips <mike@sdpdigital.com>
2021-10-25 12:32:07 -0700

    Added swift package definition

---
+++ Package.swift
@@ -0,0 +1,51 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ArcCore",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "ArcCore",
+            targets: ["ArcCore"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(
+            name: "SageResearch",
+            url: "https://github.com/Sage-Bionetworks/SageResearch.git",
+            from: "5.1.4"),
+        .package(
+            name: "SageResearch",
+            url: "https://github.com/Sage-Bionetworks/SageResearch.git",
+            from: "4.2.3"),
+        .package(
+            name: "BridgeSDK",
+            url: "https://github.com/Sage-Bionetworks/Bridge-iOS-SDK.git",
+            from: "4.4.84"),
+        .package(
+            name: "JsonModel",
+            url: "https://github.com/Sage-Bionetworks/JsonModel-Swift.git",
+            from: "1.2.3"),
+    ],
+    targets: [
+
+        .target(
+            name: "ArcCore",
+            dependencies: [
+                .product(name: "BridgeApp", package: "BridgeApp"),
+                .product(name: "BridgeAppUI", package: "BridgeApp"),
+                .product(name: "Research", package: "SageResearch"),
+                .product(name: "ResearchUI", package: "SageResearch"),
+                "BridgeSDK",
+                "JsonModel",
+            ],
+            path: "ArcCore/BridgeApp/iOS",
+            resources: [
+                .process("Localization"),
+            ]
+            ),
+    ]
+)
